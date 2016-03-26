8900e1b593204c21b4f5b1f49eb8cbc43ece1bfd
David Cole <david@team-reflex.com>
2016-03-26 14:54:20 +1300

    Added .php_cs file and ran PHP-CS-Fixer

--- example.php
+++ example.php
@@ -1,30 +1,41 @@
-<?php include __DIR__.'/vendor/autoload.php';
+<?php
+
+/*
+ * This file is apart of the DiscordPHP-BotBuilder project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+include __DIR__.'/vendor/autoload.php';
 
 use Discord\Bot\Bot;
 
 // Check for count
 if ($argc < 2) {
-	echo "Usage: php {$argv[0]} <token>\r\n";
-	die(1);
+    echo "Usage: php {$argv[0]} <token>\r\n";
+    die(1);
 }
 
 $bot = new Bot($argv[1], [
-	'prefix' => ';'
+    'prefix' => ';',
 ]);
 
 $bot->on('ready', function ($config, $discord) {
-	echo "Bot is running:\r\n";
-	echo "User: {$discord->username}#{$discord->discriminator}\r\n";
-	echo "Prefix: {$config['prefix']}\r\n";
-	echo "----------------------------------------------------\r\n";
+    echo "Bot is running:\r\n";
+    echo "User: {$discord->username}#{$discord->discriminator}\r\n";
+    echo "Prefix: {$config['prefix']}\r\n";
+    echo "----------------------------------------------------\r\n";
 });
 
 $bot->on('command-triggered', function ($command, $user) {
-	echo "Command triggered: {$command} by {$user->username}\r\n";
+    echo "Command triggered: {$command} by {$user->username}\r\n";
 });
 
 $bot->addCommand('dank', function ($params, $message) {
-	$message->reply('memes');
+    $message->reply('memes');
 });
 
 $bot->start();
--- src/Bot/Bot.php
+++ src/Bot/Bot.php
@@ -1,12 +1,20 @@
 <?php
 
+/*
+ * This file is apart of the DiscordPHP-BotBuilder project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord\Bot;
 
 use Discord\Discord;
 use Discord\WebSockets\Event;
 use Discord\WebSockets\WebSocket;
 use Evenement\EventEmitter;
-use Illuminate\Support\Arr;
 use React\EventLoop\Factory;
 
 /**
@@ -14,120 +22,120 @@ use React\EventLoop\Factory;
  */
 class Bot extends EventEmitter
 {
-	/**
-	 * The DiscordPHP instance.
-	 *
-	 * @var Discord The DiscordPHP instance.
-	 */
-	protected $discord;
+    /**
+     * The DiscordPHP instance.
+     *
+     * @var Discord The DiscordPHP instance.
+     */
+    protected $discord;
 
-	/**
-	 * The DiscordPHP WebSocket instance.
-	 *
-	 * @var WebSocket The WebSocket instance.
-	 */
-	protected $ws;
+    /**
+     * The DiscordPHP WebSocket instance.
+     *
+     * @var WebSocket The WebSocket instance.
+     */
+    protected $ws;
 
-	/**
-	 * The ReactPHP event loop.
-	 *
-	 * @var LoopInterface The event loop.
-	 */
-	protected $loop;
+    /**
+     * The ReactPHP event loop.
+     *
+     * @var LoopInterface The event loop.
+     */
+    protected $loop;
 
-	/**
-	 * The config array.
-	 *
-	 * @var array The config array.
-	 */
-	protected $config = [];
+    /**
+     * The config array.
+     *
+     * @var array The config array.
+     */
+    protected $config = [];
 
-	/**
-	 * Constructs a bot instance.
-	 *
-	 * @param string $token The bot authentication token.
-	 * @param array $config A config array.
-	 * @param LoopInterface|null $loop The ReactPHP event loop.
-	 *
-	 * @return void 
-	 */
-	public function __construct($token, array $config = [], $loop = null)
-	{
-		$this->setupConfig($config);
+    /**
+     * Constructs a bot instance.
+     *
+     * @param string             $token  The bot authentication token.
+     * @param array              $config A config array.
+     * @param LoopInterface|null $loop   The ReactPHP event loop.
+     *
+     * @return void
+     */
+    public function __construct($token, array $config = [], $loop = null)
+    {
+        $this->setupConfig($config);
 
-		$this->loop = is_null($loop) ? Factory::create() : $loop;
-		$this->discord = new Discord($token);
-		$this->ws = new WebSocket($this->discord, $this->loop, $this->config['use_etf']);
+        $this->loop = is_null($loop) ? Factory::create() : $loop;
+        $this->discord = new Discord($token);
+        $this->ws = new WebSocket($this->discord, $this->loop, $this->config['use_etf']);
 
-		$this->ws->on('ready', function ($discord) {
-			$this->emit('ready', [$this->config, $discord, $this->ws]);
+        $this->ws->on('ready', function ($discord) {
+            $this->emit('ready', [$this->config, $discord, $this->ws]);
 
-			$this->ws->on(Event::MESSAGE_CREATE, function ($message, $discord, $new) {
-				$params = explode(' ', $message->content);
-				$command = @$params[0];
-				array_shift($params); // Remove the prefix
+            $this->ws->on(Event::MESSAGE_CREATE, function ($message, $discord, $new) {
+                $params = explode(' ', $message->content);
+                $command = @$params[0];
+                array_shift($params); // Remove the prefix
 
-				foreach ($this->commands as $trigger => $listener) {
-					$expected = $this->config['prefix'].$trigger;
+                foreach ($this->commands as $trigger => $listener) {
+                    $expected = $this->config['prefix'].$trigger;
 
-					if ($command == $expected) {
-						$this->emit('command-triggered', [$expected, $message->author]);
-						call_user_func_array($listener, [$params, $message, $new, $this]);
-					}
-				}
-			});
-		});
-	}
+                    if ($command == $expected) {
+                        $this->emit('command-triggered', [$expected, $message->author]);
+                        call_user_func_array($listener, [$params, $message, $new, $this]);
+                    }
+                }
+            });
+        });
+    }
 
-	/**
-	 * Adds a command listener.
-	 *
-	 * @param string $command The command to invoke the callback on.
-	 * @param callable $listener The callback to invoke.
-	 *
-	 * @return void 
-	 */
-	public function addCommand($command, callable $listener)
-	{
-		$this->commands[$command] = $listener;
-	}
+    /**
+     * Adds a command listener.
+     *
+     * @param string   $command  The command to invoke the callback on.
+     * @param callable $listener The callback to invoke.
+     *
+     * @return void
+     */
+    public function addCommand($command, callable $listener)
+    {
+        $this->commands[$command] = $listener;
+    }
 
-	/**
-	 * Updates the config.
-	 *
-	 * @param array $config The values to update.
-	 *
-	 * @return void 
-	 */
-	public function updateConfig(array $config = [])
-	{
-		$this->config = array_merge($this->config, $config);
-	}
+    /**
+     * Updates the config.
+     *
+     * @param array $config The values to update.
+     *
+     * @return void
+     */
+    public function updateConfig(array $config = [])
+    {
+        $this->config = array_merge($this->config, $config);
+    }
 
-	/**
-	 * Sets up the config.
-	 *
-	 * @param array $config The supplied config.
-	 *
-	 * @return void 
-	 */
-	protected function setupConfig(array $config = [])
-	{
-		$defaults = [
-			'prefix' => '!',
-			'use_etf' => true,
-		];
+    /**
+     * Sets up the config.
+     *
+     * @param array $config The supplied config.
+     *
+     * @return void
+     */
+    protected function setupConfig(array $config = [])
+    {
+        $defaults = [
+            'prefix' => '!',
+            'use_etf' => true,
+        ];
 
-		$this->config = array_merge($defaults, $config);
-	}
+        $this->config = array_merge($defaults, $config);
+    }
 
-	/**
-	 * Starts the event loop.
-	 *
-	 * @return void 
-	 */
-	public function start()
-	{
-		$this->loop->run();
-	}
+    /**
+     * Starts the event loop.
+     *
+     * @return void
+     */
+    public function start()
+    {
+        $this->loop->run();
+    }
 }
